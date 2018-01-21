8c0124a64386fa7975e32df3a19a9e800811d9ad
bfelch <bfelch@users.noreply.github.com>
2018-01-20 23:01:02 -0600

    Style Changes
    Going with different design
    Adding dojo

---
+++ js/widgets/ProjectThumbnail.js
@@ -0,0 +1,40 @@
+define ([
+	'dojo/_base/declare',
+	'dijit/_WidgetBase',
+	'dijit/_TemplatedMixin',
+	'dojo/text!/js/widgets/templates/ProjectThumbnail.html'
+], function(
+		declare,
+		_WidgetBase,
+		_TemplatedMixin,
+		template) {
+	return declare('ProjectThumbnail', [_WidgetBase, _TemplatedMixin], {
+		templateString: template,
+		name: 'No Name',
+		thumbnail: '',
+		
+		constructor: function(project) {
+			this.name = project.name;
+			this.thumbnail = project.img_s;
+		},
+		
+		postCreate: function() {
+			this.inherited(arguments);
+		},
+		
+		_setNameAttr: function(name) {
+			if (name != '') {
+				this._set('name', name);
+			}
+		},
+		
+		_setThumbnailAttr: function(imagePath) {
+			if (imagePath != '') {
+				imagePath = './img/portfolio/' + imagePath;
+				
+				this._set('thumbnail', imagePath);
+				this.thumbnailNode.src = imagePath;
+			}
+		}
+	});
+});
